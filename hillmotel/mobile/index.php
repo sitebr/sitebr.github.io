27486aaeaa82d5c7611502488a1f27f09fd5608d
Anderson <114543978+sitebr@users.noreply.github.com>
2022-12-08 21:04:57 -0300

    first action

---
+++ hillmotel/mobile/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VL3HNPB829"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-VL3HNPB829');
+    </script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="O melhor motel da Região dos Lagos. Venha conhecer! Suítes com hidromassagem a partir de R$ 91,00!" />
+    <meta name="robots" content="index, follow">
+    <meta name="keywords" content="motel, hotel, pousada, região dos lagos, suítes, hidromassagem, piscina, pernoite, lua de mel, aniversário, casamento, prazer, amor"/>
+    <link rel="canonical" href="http://www.hillmotel.com.br">
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="css/style.css">
+    <meta http-equiv="refresh" content="">
+    <!-- <meta http-equiv="cache-control" content="no-cache, no-store"> -->
+    <!-- <meta http-equiv="Pragma" content="no-cache, no-store"> -->
+    <title>Motel Cravo e Canela</title>
+</head>
+<body>
+    <script>
+        if (window.screen.width > 600 ) {
+        location.href = '../'
+        }
+    </script>
+    <?php
+        include('html/cabecalho.html')
+    ?>
+    <iframe class="myIframe" src="html/inicio.html" frameborder="0"></iframe>
+
+    <script src="script/script.js"></script>
+</body>
